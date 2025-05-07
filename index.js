fe7b698d72ab14fa543358ed21affb5fb186ca40
anais <anaisflores@tecsup.com>
2025-05-07 13:54:15 -0500

    Configuración con dotenv, express y nodemon

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+require('dotenv').config() 
+const http= require('http') 
+function requestController(){ 
+console.log('Bienvenidos al curso') 
+} 
+const server=http.createServer (requestController) 
+const PORT=process.env.PORT 
+server.listen (PORT, function(){ 
+    console.log(`Aplicación corriendo en el puerto: ${PORT}`);
+});
 // Archivo de inicio 
